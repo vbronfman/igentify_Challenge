a19a40cf4df08db5e71523e6dce27f024698aebf
vladbronfman <vlad.bronfman@gmail.com>
2020-11-15 23:24:34 +0200

    deploy.sh with kubectl expose commands

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,11 @@
-docker build -t vladbronfman/multi-client:latest -t vladbronfman/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t vladbronfman/multi-server:latest -t vladbronfman/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t vladbronfman/multi-worker:latest -t vladbronfman/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t vladbronfman/igentify_challenge_web:latest -t vladbronfman/igentify_challenge_web:$SHA -f ./composetest/Dockerfile ./composetest
 
-docker push vladbronfman/multi-client
-docker push vladbronfman/multi-server
-docker push vladbronfman/multi-worker
+docker push vladbronfman/igentify_challenge_web
 
-docker push vladbronfman/multi-client:$SHA
-docker push vladbronfman/multi-server:$SHA
-docker push vladbronfman/multi-worker:$SHA
+docker push vladbronfman/igentify_challenge_web:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployment/server-deployment server=vladbronfman/multi-server:$SHA
-kubectl set image deployment/client-deployment client=vladbronfman/multi-client:$SHA
-kubectl set image deployment/worker-deployment worker=vladbronfman/multi-worker:$SHA
+kubectl expose deployment redis  --type=ClusterIP --name=redis
+kubectl expose deployment web-deployment --type=LoadBalancer --name=web-service
+kubectl set image deployment/web-deployment web=vladbronfman/igentify_challenge_web:$SHA
+
